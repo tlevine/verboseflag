b48ac4d10abe714ca5ed6f7b593dac7e2fbdc627
Thomas Levine <_@thomaslevine.com>
2015-08-22 16:20:00 -0400

    j

--- verboseflag/configuration.py
+++ verboseflag/configuration.py
@@ -1,22 +1,36 @@
 import logging
 
-def configure_logging(verbosity):
-    my_levels = [
-        logging.WARN,
-        logging.INFO,
-        logging.DEBUG,
-        logging.DEBUG,
-        logging.DEBUG,
-        logging.DEBUG,
-    ]
-    upstream_levels = [
-        logging.ERROR,
-        logging.ERROR,
-        logging.ERROR,
-        logging.WARN,
-        logging.INFO,
-        logging.DEBUG,
-    ]
+my_levels = [
+    logging.WARN,
+    logging.INFO,
+    logging.DEBUG,
+    logging.DEBUG,
+    logging.DEBUG,
+    logging.DEBUG,
+]
+upstream_levels = [
+    logging.ERROR,
+    logging.ERROR,
+    logging.ERROR,
+    logging.WARN,
+    logging.INFO,
+    logging.DEBUG,
+]
+
+class VerbosityFilter:
+    def __init__(self, verbosity, package_list):
+        self.verbosity = verbosity
+        self.packages = set(package_list)
+
+    @classmethod
+    def filter(record):
+        is_my_package = any(record.name.startswith(name) for name in packages):
+        upstream_levels[verbosity]
+        past_level = record.level >= 
+
+
+def configure_logging(verbosity, package_list):
+    packages = set(package_list)
 
     for handler in logging.root.handlers:
         pass
