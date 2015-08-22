2fbe6162947212fcb76b6090f20d1ba4199590c5
Thomas Levine <_@thomaslevine.com>
2015-08-22 13:16:09 -0400

    start log configuration

--- verboseflag/configuration.py
+++ verboseflag/configuration.py
@@ -1,2 +1,25 @@
+import logging
+
 def configure_logging(verbosity):
-    
+    my_levels = [
+        logging.WARN,
+        logging.INFO,
+        logging.DEBUG,
+        logging.DEBUG,
+        logging.DEBUG,
+        logging.DEBUG,
+    ]
+    upstream_levels = [
+        logging.ERROR,
+        logging.ERROR,
+        logging.ERROR,
+        logging.WARN,
+        logging.INFO,
+        logging.DEBUG,
+    ]
+
+    for handler in logging.root.handlers:
+        pass
+
+    # Next line is wrong.
+    logging.basicConfig(level = my_levels[verbosity])
