408bd7eac82524f56ae69ad58d02ac8b0f6a88ae
Thomas Levine <_@thomaslevine.com>
2015-11-09 15:52:49 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='verboseflag',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Configure verbosity',
+      url='http://dada.pink/!/verboseflag/',
+      packages=['verboseflag'],
+      install_requires = [
+      ],
+      tests_require = [
+      ],
+      version='0.0.1',
+      license='LGPL',
+)
